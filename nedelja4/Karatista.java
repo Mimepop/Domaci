a88648eca01105390eda144f01a060a8aded9aec
Mimepop <mime.p.95@gmail.com>
2021-08-18 20:30:13 +0200

    Initial commit - Homework for week 4

---
+++ nedelja4/Karatista.java
@@ -0,0 +1,68 @@
+package nedelja4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Karatista {
+    private String ime;
+    private int godine;
+    private String rang;
+    private String[] kata;
+
+    public Karatista(String ime, int godine, String rang, String[] kata) {
+        this.ime = ime;
+        this.godine = godine;
+        this.rang = rang;
+        this.kata = kata;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public int getGodine() {
+        return godine;
+    }
+
+    public void setGodine(int godine) {
+        this.godine = godine;
+    }
+
+    public String getRang() {
+        return rang;
+    }
+
+    public void setRang(String rang) {
+        this.rang = rang;
+    }
+
+    public String getKata(int index) {
+        return kata[index];
+    }
+
+    public void setKata(String[] kata) {
+        this.kata = kata;
+    }
+
+    public void napadni(int index) {
+           System.out.println( "Karatista " + this.ime + " napada sa " + this.getKata(index) + " katom");
+    }
+
+    public void vatreniNapad() {
+        int randIdx = new Random().nextInt((this.kata.length-1) - 0 + 1);
+        String randElem = kata[randIdx];
+        System.out.println("Karatista " + this.ime + " napada sa " + randElem + " katom.");
+    }
+
+    // Napraviti metod napadni(int index), koja za prosledjeni indeks ispisuje poruku: "Karatista {ime} napada sa {kata[index]} katom".
+    //Napraviti metod vatreniNapad(), koja ispisuje istu poruku kao i napadni() metod, osim sto bira nasumicnu katu.
+    public void log() {
+        System.out.println("Karatista: " + this.ime + ", " + this.godine + ", " + this.rang + " - zna naredne kate " + Arrays.toString(this.kata));
+    }
+
+}
+
