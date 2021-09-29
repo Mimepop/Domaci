b85dca9f6716af1e4aa24521a68ffb2946c6870a
Milena Popovic <84718107+Mimepop@users.noreply.github.com>
2021-09-29 13:46:17 +0200

    Update FoodDao.java

--- nedelja7/Nedelja/dao/FoodDao.java
+++ nedelja7/Nedelja/dao/FoodDao.java
@@ -2,12 +2,15 @@ package rs.itbootcamp.dao;
 
 import rs.itbootcamp.model.FoodModel;
 
-import java.sql.SQLException;
 import java.util.List;
 
+
 public interface FoodDao {
-    void insert(FoodModel fm);
-    void delete(int id) throws SQLException;
+    void add(FoodModel fm);
+    void addToMeal(int meal_id, int food_id, double mass);
+    void addToFridge(int fridge_id, int food_id, double mass);
+    void removeFromFridge(int food_id);
+    void delete(int id);
     void update(FoodModel fm);
     FoodModel getFood(int id);
     List<FoodModel> getAllFood();
