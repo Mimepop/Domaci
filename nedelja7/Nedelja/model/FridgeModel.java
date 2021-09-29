d3b8a922f0585be6e5b752a183f5c644e1404773
Milena Popovic <84718107+Mimepop@users.noreply.github.com>
2021-09-29 13:53:23 +0200

    Create FridgeModel.java

---
+++ nedelja7/Nedelja/model/FridgeModel.java
@@ -0,0 +1,36 @@
+package rs.itbootcamp.model;
+
+
+public class FridgeModel {
+    private int fridgeId;
+    private int userId;
+
+    public FridgeModel(int fridgeId, int userId) {
+        this.fridgeId = fridgeId;
+        this.userId = userId;
+    }
+
+    public int getFridgeId() {
+        return fridgeId;
+    }
+
+    public void setFridgeId(int fridgeId) {
+        this.fridgeId = fridgeId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "FridgeModel{" +
+                "fridgeId=" + fridgeId +
+                ", userId=" + userId +
+                '}';
+    }
+}
